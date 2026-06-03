c6bcfbd2f51a39eb63d78570a683dabddf09d0e6
Rudra Pratap Singh <rudrapratapsingh.fzd@gmail.com>
2026-06-03 16:18:51 +0530

    chore: add boilerplate files and utils

---
+++ app.js
@@ -0,0 +1,15 @@
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.json({ message: "DevHire API is running" });
+});
+
+export default app;
