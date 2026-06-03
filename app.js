ad1ac3410b974836b742091b4445aa2bce139a2b
Rudra Pratap Singh <rudrapratapsingh.fzd@gmail.com>
2026-06-04 04:19:37 +0530

    feat: jwt auth with register, login, logout and refresh

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
+import authRouter from "./src/routes/auth.routes.js";
 
 const app = express();
 
@@ -8,11 +9,20 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-import authRouter from "./src/routes/auth.routes.js";
 app.use("/api/auth", authRouter);
 
 app.get("/", (req, res) => {
   res.json({ message: "DevHire API is running" });
+});
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Something went wrong";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
 
 export default app;
